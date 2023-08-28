d889cef632bda95749b1daeabae77fef63c6eceb
Rei-Konomiya <reikonomiya56@gmail.com>
2023-08-29 00:19:01 +0900

    [disable] TOKEN deleated.

--- token.h
+++ token.h
@@ -1,6 +1,5 @@
-// WiFi設定
-const char* ssid     = "C0p2Ec2-WLAN";
-const char* password = "4Emah5LdS";
-
-// LINE Notify設定
-const char* token = "";//zfSxNvQD5NeRqf7SiGG2WM9ph9hhMxCjMIjxUPy6UHe";
+// WiFi
+const char* ssid     = YOUR_SSID;
+const char* password = YOUR_PASSWORD;
+// LINE Notify
+const char* token = YOUR_LINE_TOEN;
